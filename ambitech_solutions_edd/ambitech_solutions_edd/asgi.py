ca80110922eec4bebacef27a411824111b425f92
Vitor <vitorqlucas@gmail.com>
2025-01-14 18:26:13 -0300

    Starting a Django Project

---
+++ ambitech_solutions_edd/ambitech_solutions_edd/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for ambitech_solutions_edd project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ambitech_solutions_edd.settings')
+
+application = get_asgi_application()
